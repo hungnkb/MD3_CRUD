175a5095c08e871c3e78b79441d5348436d52252
unknown <hungnkb>
2023-01-23 14:59:30 +0700

    first commit

---
+++ controller/handlers.js
@@ -0,0 +1,163 @@
+const fs = require('fs');
+const qs = require('qs')
+const http = require('http')
+
+let handlers = {};
+handlers.showProducts = (req, res) => {
+    let html = '';
+    fs.readFile('./data/data.json', 'utf8', (err, data) => {
+        if (err) {
+            console.log(err.message);
+        } else {
+            data = JSON.parse(data);
+            data.forEach((item) => {
+                html += `
+                <tr>
+                    <td>${item.id}</td>
+                    <td>${item.name}</td>
+                    <td>${item.price}</td>
+                    <td><a onclick="return confirm('Are you sure to delete this item?')" href="/delete?id=${item.id}"><button type="button" class="btn btn-danger">Delete</button></a> <a href="/edit?id=${item.id}"><button type="button" class="btn btn-primary">Update</button></a></td>
+                </tr>
+                `
+            });
+            fs.readFile('./views/products.html', 'utf8', (err, data) => {
+                if (err) {
+                    console.log(err.message);
+                } else {
+                    data = data.replace('{item-list}', html);
+                    res.write(data);
+                    res.end()
+                }
+            });
+
+        }
+    })
+}
+
+handlers.addItem = (req, res) => {
+    let newItem = '';
+    req.on('data', chunk => {
+        newItem += chunk;
+    })
+    req.on('end', () => {
+        let itemList = '';
+        fs.readFile('./data/data.json', 'utf-8', (err, data) => {
+            if (err) {
+                console.log(err.message)
+            } else {
+                itemList += data
+                itemList = JSON.parse(itemList)
+                newItem = qs.parse(newItem);
+                let maxId = 0;
+                for (let i of itemList) {
+                    if (i.id > maxId) {
+                        maxId = i.id;
+                    }
+                }
+                console.log(maxId)
+                newItem.id = maxId + 1;
+                itemList.push(newItem);
+                fs.writeFile('./data/data.json', JSON.stringify(itemList), err => {
+                    if (err) {
+                        console.log(err.message)
+                    } else {
+                        res.writeHead(301, { location: '/products' });
+                        res.end()
+                    }
+                });
+            }
+        })
+    })
+}
+
+
+handlers.showAddItemPage = (req, res) => {
+    fs.readFile('./views/additem.html', 'utf-8', (err, data) => {
+        if (err) {
+            console.log(err.message);
+        } else {
+            res.end(data)
+        }
+    })
+}
+
+handlers.delete = (idDelete, req, res) => {
+    let itemList = ''
+    fs.readFile('./data/data.json', 'utf-8', (err, data) => {
+        if (err) {
+            console.log(err.message)
+        } else {
+            itemList = JSON.parse(data);
+        }
+        let index = -1
+        for (let i in itemList) {
+            if (itemList[i].id == idDelete) {
+                index = i;
+            }
+        }
+        if (index == -1) {
+            res.writeHead(404, 'Not Found')
+            res.end()
+        } else {
+            itemList.splice(index, 1)
+            fs.writeFile('./data/data.json', JSON.stringify(itemList), err => {
+                if (err) {
+                    console.log(err.message)
+                } else {
+                    res.writeHead(301, { location: '/products' });
+                    res.end()
+                }
+            })
+        }
+    })
+}
+
+handlers.showEditItemPage = (id, req, res) => {
+    fs.readFile('./views/editItem.html', 'utf-8', (err, data) => {
+        if (err) {
+            console.log(err.message);
+        } else {
+            res.end(data)
+        }
+    })
+}
+
+handlers.edit = (idEdit, req, res) => {
+    let itemList = '';
+    let newItem = ''
+    req.on('data', chunk => {
+        newItem += chunk;
+    })
+    req.on('end', () => {
+        fs.readFile('./data/data.json', (err, data) => {
+            if (err) {
+                console.log(err.message);
+            } else {
+                itemList = JSON.parse(data);
+                let index = -1;
+                for (let i in itemList) {
+                    if (itemList[i].id == idEdit) {
+                        index = i;
+                    }
+                }
+                newItem = qs.parse(newItem);
+                newItem.id = idEdit;
+                itemList.splice(index, 1, newItem);
+                fs.writeFile('./data/data.json', JSON.stringify(itemList), err => {
+                    if (err) {
+                        console.log(err.message)
+                    } else {
+                        res.writeHead(301, { location: '/products' });
+                        res.end();
+                    }
+                })
+            }
+        })   
+    })
+    
+}
+
+module.exports = {
+    handlers
+}
+
---
+++ server.js
@@ -0,0 +1,39 @@
+const http = require('http');
+const fs = require('fs');
+const qs = require('qs');
+const url = require('url')
+const { handlers } = require('./controller/handlers')
+
+const server = http.createServer((req, res) => {
+    let getUrl = url.parse(req.url, true);
+    let id = qs.parse(getUrl.query).id
+    switch (getUrl.pathname) {
+        case '/products':
+            handlers.showProducts(req, res);
+            break;
+        case '/addItem':
+            if (req.method == 'GET') {
+                handlers.showAddItemPage(req, res);
+            } else {
+                handlers.addItem(req, res);
+            };
+            break;
+        case '/delete':
+            handlers.delete(id ,req, res);
+            break;
+        case '/edit':
+            if (req.method == 'GET') {
+                handlers.showEditItemPage(id, req, res);
+            } else {
+                handlers.edit(id, req, res);
+            };
+           
+            break;
+        default:
+            res.end('home')
+    }
+})
+
+server.listen(8080, () => {
+    console.log('Server is running at localhost:8080');
+})
